848deeb1f8a1dca57f54455d0f94f6b1ee6c9c0c
baikauskaite <baikauskaitev@gmail.com>
2022-04-08 15:16:55 +0200

    Fix the 'b' in server message and match heading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import socket
+from server_message import *
 
 BUFFER_SIZE = 2048
 HOST_NAME = os.getenv("HOST")
@@ -8,14 +9,21 @@ PORT_NUMBER = int(os.getenv("PORT"))
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST_NAME, PORT_NUMBER))
 
-# First hand-shake message
+# First hand-shake message, encode it in bytes
 buffer_str = "HELLO-FROM Viktorija\n"
 buffer_bytes = buffer_str.encode()
 
-num_bytes_sent = s.send(buffer_bytes)
+# Send bytes to server
 s.sendall(buffer_bytes)
 
+# Receive bytes from server and decodes it
 buffer_str = s.recv(BUFFER_SIZE)
-print(buffer_str)
+decoded_str = buffer_str.decode()
+
+# Parse the server message and respond accordingly
+server_message = ServerMessage(decoded_str)
+server_message.match_heading()
+
+print(decoded_str)
 
 s.close()
