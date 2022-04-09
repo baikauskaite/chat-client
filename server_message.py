7ec37019856585ac663a2972f2d190a5c61a93a9
baikauskaite <baikauskaitev@gmail.com>
2022-04-09 18:40:08 +0200

    Define the behaviour for server messages * HELLO * SEND-OK * UNKNOWN * DELIVERY * IN-USE * BUSY

--- server_message.py
+++ server_message.py
@@ -8,20 +8,51 @@ class ServerMessage:
         self.body = word_list
 
     # Server's response to a correct first-handshake
+    # Returns a code which can be used by the controller to determine that there was not failure in the process
+    # TODO: There's an error in the concatenation
     # TODO: newlines should be removed from the request's body
-    def second_handshake(self):
-        print("Hello, " + self.body)
+    def second_handshake(self) -> int:
+        username = self.body[0]
+        print("You have successfully logged in, " + username + ".")
+        return 1
+
+    def send_ok(self) -> int:
+        print("The message has been sent successfully.")
+        return 2
+
+    def unknown(self) -> int:
+        print("This user is currently not logged in.")
+        return -3
+
+    def delivery(self) -> int:
+        username = self.body[0]
+        message = self.body[1]
+        print(username + ": " + message)
+        return 2
+
+    def in_use(self) -> int:
+        print("This username is already in use.")
+        return -1
+
+    def busy(self) -> int:
+        print()
+        return -2
 
     # Pairs of headings and functions to process the body for the matching heading
     headings = {
-        "HELLO": second_handshake
+        "HELLO": second_handshake,
+        "SEND-OK": send_ok,
+        "UNKNOWN": unknown,
+        "DELIVERY": delivery,
+        "IN-USE": in_use,
+        "BUSY": busy
     }
 
     # Match heading with a function to process the message's body
     # TODO: else case, when there's no matching heading in headings
-    def match_heading(self):
+    def match_heading(self) -> int:
         function = self.headings[self.head]
         if function is not None:
-            function(self)
+            return function(self)
         else:
             pass
