0d76d8371812f68ff9db4086df781486e26a36e2
Viktorija <baikauskaitev@gmail.com>
2022-04-10 18:33:39 +0200

    Server message v2 (#3)
    * Fix server_message message splitting and function call
    
    * Fix the 'b' in server message and match heading

--- server_message.py
+++ server_message.py
@@ -1,21 +1,27 @@
 class ServerMessage:
     """Server message is split to head and body. The body can then processed according to the heading."""
 
+    # Splitting the message into a heading and a body
     def __init__(self, message):
         word_list = message.split()
-        self.head = word_list[0]
-        self.body = word_list.pop(0)
+        self.head = word_list.pop(0)
+        self.body = word_list
 
+    # Server's response to a correct first-handshake
+    # TODO: newlines should be removed from the request's body
     def second_handshake(self):
-        pass
+        print("Hello, " + self.body)
 
+    # Pairs of headings and functions to process the body for the matching heading
     headings = {
         "HELLO": second_handshake
     }
 
+    # Match heading with a function to process the message's body
+    # TODO: else case, when there's no matching heading in headings
     def match_heading(self):
-        value = self.headings[self.head]
-        if value is None:
-            pass
+        function = self.headings[self.head]
+        if function is not None:
+            function(self)
         else:
-            value()
+            pass
