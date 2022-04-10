8e68c6011c2e576fcd540128cb8c7fb6047236b3
P Campbell <71976650+paigecache@users.noreply.github.com>
2022-04-11 01:35:45 +0200

    some initial controller  progress on my end

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 import re
 from server_message import *
+from client_message import *
 
 
 class Controller:
@@ -10,9 +11,11 @@ class Controller:
 
     def __init__(self, socket):
         self.socket = socket
+        self.client = ClientMessage(socket)
+        self.server = ServerMessage(socket)
 
     # User manual that is displayed in the beginning and when called by writing '!help'
-    def help(self):
+    def help(self) -> None:
         print("""
         @username message - to send a message to a user.        
         !who              - to list all users that are currently logged in.
@@ -22,47 +25,54 @@ class Controller:
 
     user_commands = {
         "!who": lambda x: print("This code is not yet implemented."),
-        "!quit": lambda x: print("This code is not yet implemented."),
-        "!help": help
+        "!quit": quit_program(),
+        "!help": help()
     }
 
+    # tells user program is quitting and quits
+    def quit_program(self) -> None:
+        self.socket.close()
+        print("Quitting program.")
+        quit()
+
+    # parses what the user enters and begins the appropriate process
     # TODO: Add case for '@username message'
     def parse_user_input(self):
         user_input = input()
+        if user_input[0] == "@":
+            # separate username from body, remove @, process
+            pass
         while user_input not in self.user_commands:
             user_input = input("There's no such command.")
-        return self.user_commands[user_input](self)
+        self.user_commands[user_input](self)
 
     # Prompts the user to select a username and initiates handshake with server
-    def login(self):
+    def login(self) -> None:
         username = input("Please choose a username: ")
         while not re.match(self.USERNAME_REGEX, username):
             print("Your username should consist of 2-20 alphanumeric characters.")
             username = input("Please choose a username: ")
-        # TODO: Change the string below in the brackets to a ClientMessage object
-        self.interact_with_server("HELLO-FROM " + username + "\n")
+        self.interact_with_server(self.client.first_handshake(username))
 
     # Refer to the class ServerMessage to see what each number refers to
     # Negative numbers indicate some kind of failure, positive indicate a successful process
     processes = {
-        -2: lambda x: print("This code is not yet implemented."),
+        # too many clients in server
+        -2: lambda x: print("Too many clients currently logged in. Try again later."),
+        # username taken, login again
         -1: login,
+        # successful command, user enter another command
         1: parse_user_input
     }
 
     # Send client message and receive server message, continue the interaction with user according to server response
-    def interact_with_server(self, message):
-        self.send_client_message(message)
+    # second parameter is a client method
+    def interact_with_server(self, clientMethodToRun):
+        clientMethodToRun()
         server_message = ServerMessage(self.socket)
         code = server_message.code
         # Calls a function corresponding to the code
         if code in self.processes:
             self.processes[code](self)
         else:
-            print("This code is not yet implemented.")
-
-    # Encode a message and send it to the server
-    # TODO: This should be a method of ClientMessage class
-    def send_client_message(self, message):
-        message_bytes = message.encode()
-        self.socket.sendall(message_bytes)
+            print("This should not get called.")
