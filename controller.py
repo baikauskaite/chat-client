2d08c55e68e03f4cb948c2c1411eafe306fd0b9f
baikauskaite <baikauskaitev@gmail.com>
2022-04-10 11:41:29 +0200

    Create help message and dictionary of user commands

--- controller.py
+++ controller.py
@@ -11,6 +11,20 @@ class Controller:
     def __init__(self, socket):
         self.socket = socket
 
+    # User manual that is displayed in the beginning and when called by writing '!help'
+    @staticmethod
+    def help():
+        print("""
+        @username message - to send a message to a user.        
+        !who              - list all users that are currently logged in.
+        !quit             - to shutdown the client.
+        !help             - to display the manual
+        """)
+
+    user_commands = {
+        "!help": help
+    }
+
     # Prompts the user to select a username and initiates handshake with server
     def login(self):
         username = input("Please choose a username: ")
@@ -19,6 +33,12 @@ class Controller:
             username = input("Please choose a username: ")
         # TODO: Change the string below in the brackets to a ClientMessage object
         self.interact_with_server("HELLO-FROM " + username + "\n")
+
+    # Refer to the class ServerMessage to see what each number refers to
+    # Negative numbers indicate some kind of failure, positive indicate a successful process
+    processes = {
+        -1: login
+    }
 
     # Send client message and receive server message, continue the interaction with user according to server response
     def interact_with_server(self, message):
@@ -30,11 +50,6 @@ class Controller:
             self.processes[code]()
         else:
             print("This code is not yet implemented.")
-
-    # Refer to the class ServerMessage to see what each number refers to
-    processes = {
-        -1: login
-    }
 
     # Encode a message and send it to the server
     # TODO: This should be a method of ClientMessage class
