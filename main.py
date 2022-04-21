851987cdea9622197cf5adabcd6d52f6e9153651
baikauskaite <baikauskaitev@gmail.com>
2022-04-21 09:37:51 +0300

    Add comment about HOST and PORT

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 from controller import *
 
 BUFFER_SIZE = 2048
+# Set up HOST and PORT environmental variables before running
 HOST_NAME = os.getenv("HOST")
 PORT_NUMBER = int(os.getenv("PORT"))
 
 controller = Controller(HOST_NAME, PORT_NUMBER)
-# This function is responsible for the whole flow of the chat
 controller.run_chat()
